6e88e5af4c4de6f3d91d39ec99a0f73abdc5fce8
Ahmet Sezen <sezenahmeet@gmail.com>
2020-11-17 11:41:31 +0200

    Update ApiConfig.js

--- src/Api/ApiConfig.js
+++ src/Api/ApiConfig.js
@@ -1,6 +1,6 @@
 export const ApiConfig = {
     headers: {
         "Client-ID": "gp762nuuoqcoxypju8c569th9wz7q5",
-        "Authorization": "Bearer ck03jhd52zb5737areyp942l9mnfs3"
+        "Authorization": "Bearer ck03jhd52zb5737areyp942l9mnfs3",
     }
 }
